3a9d293eb441fbb3202c04e68142c3edf5710da9
Marina Bondarenko <115567987+MarinaBond@users.noreply.github.com>
2022-11-16 22:10:45 +0300

    Update task5.py

--- Лабораторная 5/task5.py
+++ Лабораторная 5/task5.py
@@ -3,10 +3,10 @@ from random import sample
 import string
 
 
-def get_random_password() -> str:
+def get_random_password(n=8) -> str:
     w = string.ascii_uppercase + string.digits + string.ascii_lowercase
     random_pass = ""
-    x = random.sample(w, 8)
+    x = random.sample(w, n)
     random_pass += ''.join(x)
 
     return random_pass
